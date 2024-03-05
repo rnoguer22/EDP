af389b716a0013aa51295c6488e1d6e5ceef079a
rnoguer22 <rnogugon@myuax.com>
2024-03-05 17:21:48 +0100

    EDP de Ondas

--- EDPOndas.py
+++ EDPOndas.py
@@ -3,34 +3,40 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 a = 0
-b = np.pi
+b = 5
 c = 0
-d = np.pi
+d = 10
 N = 40
-M = 40
+M = 400
 
-w = np.zeros((N+1, M+1))
+w = np.zeros((M+1, N+1))
 
 h = (b - a) / N
 k = (d - c) / M
 
-def f(i, j):
-    #return (a+h*i)*(c+k*j)*(1-(a+h*i))*(1-(c+k*j))
+v = 1.5
+
+p = v*k/h #Longitud de la cuerda
+
+
+def f(x):
+    return x*(b-x)
+
+def g(x):
     return 0
 
+for j in range(1, M):
+    w[j][0] = 0
+    w[j][N] = 0
+
 for i in range(1, N):
-    w[0][i] = 0
-    w[M][i] = 0
+    w[0][i] = f(h*i)
+    w[1][i] = w[0][i] + k*g(h*i)
+
 
 for j in range(1, M):
-    w[j][0] = c+k*j
-    w[j][N] = c+k*j
-
-
-for _ in range(100):
     for i in range(1, N):
-        for j in range(1, M):
-            w[i][j] = (k**2 * (w[i+1][j] + w[i-1][j]) + h**2 * (w[i][j+1] + w[i][j-1]) - (h*k)**2 * f(i, j)) / (2 * (h**2 + k**2))
+        w[j+1][i] = 2*(1-((v*k/h)**2)) * w[j][i] + ((v*k/h)**2) * (w[j][i+1] + w[j][i-1]) - w[j-1][i] 
 
 x = np.linspace(a, b, N+1)
 y = np.linspace(c, d, M+1)
